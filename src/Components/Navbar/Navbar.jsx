d972d29eae16b628e6b3fe4647fa1e36f577ffec
Farangiz17 <farangizismatova24@gmail.com>
2023-05-23 21:25:56 +0500

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { navbar } from '../../utilts/navbar/Navbar'
+
+export default function Navbar() {
+    console.log(navbar);
+  return (
+    <div>
+        
+    </div>
+  )
+}
